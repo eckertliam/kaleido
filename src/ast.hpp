a17b4dad0e90dcca248839c9e06d23d0e2cd9709
Liam Eckert <liameckert17@gmail.com>
2025-08-03 22:07:30 -0400

    Working basic codegen

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,11 +2,15 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Function.h>
+
 
 // base class for expr nodes
 class ExprAST {
 public:
     virtual ~ExprAST() = default;
+    virtual llvm::Value *codegen() = 0;
 };
 
 // expr class for numeric literals
@@ -15,6 +19,7 @@ class NumberExprAst : public ExprAST {
 
 public:
     explicit NumberExprAst(const double Val) : Val(Val) {}
+    llvm::Value *codegen() override;
 };
 
 // expr class for referencing a var
@@ -23,6 +28,7 @@ class VariableExprAst : public ExprAST {
 
 public:
     explicit VariableExprAst(std::string  Name) : Name(std::move(Name)) {}
+    llvm::Value *codegen() override;
 };
 
 
@@ -34,6 +40,7 @@ class BinaryExprAst : public ExprAST {
 public:
     BinaryExprAst(const char Op, std::unique_ptr<ExprAST> Lhs, std::unique_ptr<ExprAST> Rhs)
         : Op(Op), Lhs(std::move(Lhs)), Rhs(std::move(Rhs)) {}
+    llvm::Value *codegen() override;
 };
 
 class CallExprAst : public ExprAST {
@@ -43,6 +50,7 @@ class CallExprAst : public ExprAST {
 public:
     CallExprAst(std::string  Callee, std::vector<std::unique_ptr<ExprAST>> Args)
         : Callee(std::move(Callee)), Args(std::move(Args)) {}
+    llvm::Value *codegen() override;
 };
 
 // represents the prototype for a function
@@ -56,6 +64,7 @@ public:
         : Name(std::move(Name)), Args(std::move(Args)) {}
 
     [[nodiscard]] const std::string& getName() const { return Name; }
+    [[nodiscard]] llvm::Function *codegen() const;
 };
 
 // represents a function definition itself
@@ -66,4 +75,5 @@ class FunctionAST {
 public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
         : Proto(std::move(Proto)), Body(std::move(Body)) {}
+    [[nodiscard]] llvm::Function *codegen() const;
 };
